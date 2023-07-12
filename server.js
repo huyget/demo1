1d7711d792571ac160b49d4a491780ca904bb142
huy-get <vovanhuy7497@gmail.com>
2023-07-12 16:31:58 +0700

    bai 1

--- server.js
+++ server.js
@@ -1,2 +1,14 @@
-console.log("hello world!");
-console.log("helojoids")
+const express = require('express')
+const app = express()
+const port = 8080
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+app.get('/about', (req, res) => {
+    res.send('hahha')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
